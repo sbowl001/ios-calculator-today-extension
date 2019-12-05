d747b64cc07c7b9cf56bbb823d62fc6263e79394
Stephanie <sbowl001@fiu.edu>
2019-12-05 17:34:56 +0800

    interface for widget

---
+++ RPN Calculator/Calculator/Calculator Widget/TodayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TodayViewController.swift
+//  Calculator Widget
+//
+//  Created by Stephanie Bowles on 12/5/19.
+//  Copyright © 2019 Stephanie Bowles. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.extensionContext?.widgetLargestAvailableDisplayMode = .expanded
+    }
+        
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
